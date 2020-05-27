30c0c95f5beeacf6bfdc0adfab0eab47dab497a4
Greg Ingersoll <greg.ingersoll@convolutionresearch.com>
2020-05-27 14:19:38 -0500

    :wrench: Update setup metadata and version

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 from setuptools import setup
+from os import path
+
+# Read the contents of the README file
+cwd = path.abspath(path.dirname(__file__))
+with open(path.join(cwd, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name="svg",
-    version="0.0.1",
+    name="pysvglib",
+    version="0.3.2",
     description="SVG drawing library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Multimedia :: Graphics',
+    ],
+    keywords='svg graphics',
+    url='https://github.com/gbingersoll/pysvglib',
+    author='Greg Ingersoll',
+    author_email='greg.ingersoll@convolutionresearch.com',
+    license='MIT',
     packages=['svg'],
     extras_require={
-        'dev': ['pytest', 'pycodestyle']
+        'dev': ['pytest', 'pycodestyle', 'setuptools', 'wheel']
     }
 )
